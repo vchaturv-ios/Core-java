dc5754ca23f9a807a994ab928e1ccf7d17000516
Vishnu Chaturvedi <59867319+vchaturv-cn@users.noreply.github.com>
2021-04-22 22:50:44 +0530

    Array Medium Level Programs added.

---
+++ Basic-programs/Arrays/SwapHalfArray.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SwapHalfArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[(arr.length + 1) / 2 + i];
+            arr[(arr.length + 1) / 2 + i] = temp;
+        }
+        System.out.println("Swapper Array: " + Arrays.toString(arr));
+    }
+}
