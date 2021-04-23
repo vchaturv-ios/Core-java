7301948c695327ea9627612668a34f40cfcc0d26
Vishnu Chaturvedi <vishnu.chaturvedi.1002@gmail.com>
2021-04-23 18:51:17 +0530

    Arrays Programs for Missing elements

---
+++ Basic-programs/Arrays/MissingElements.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class MissingElements {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 6, 8, 12};
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = arr[i]+1; j < arr[i + 1]; j++) {
+                System.out.println(j);
+            }
+        }
+    }
+}
---
+++ Basic-programs/Arrays/MissingElementsBetweenRange.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class MissingElementsBetweenRange {
+    public static void main(String[] args) {
+        int[] arr = {8, 28, 15, 5, 32, 56, 14};
+        int[] missing_arr = new int[100];
+
+        for (int i = 0; i < arr.length; i++) {
+            missing_arr[arr[i]] += 1;
+        }
+
+        for (int i = 0; i < missing_arr.length; i++) {
+            if (missing_arr[i] < 1 && i > 0)
+                System.out.println(i);
+        }
+
+    }
+}
